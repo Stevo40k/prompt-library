190c5378f7ce6f4bdaf8ee9ea076fcc261c2f4ea
Steven Klenklen <stevo40k@gmail.com>
2026-05-01 20:27:44 -0700

    feat: add left navigation drawer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { PromptDashboardComponent } from './prompt-dashboard/prompt-dashboard.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PromptDashboardComponent],
+  imports: [
+    PromptDashboardComponent,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
